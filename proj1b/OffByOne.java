90d9b2fb26109caa11fb22160d54b3b777ae193a
Stromxun <1512144613@qq.com>
2024-01-01 14:06:32 +0800

    revive OffByOne

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -3,8 +3,12 @@ public class OffByOne implements CharacterComparator {
 
 
 
+    private int diff(int x, int y) {
+        return Math.abs(x - y);
+    }
+
     @Override
     public boolean equalChars(char x, char y) {
-        return Math.abs((int) x - (int) y) == 1;
+        return diff(x, y) == 1;
     }
 }
