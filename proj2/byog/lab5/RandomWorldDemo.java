4ba308c80c3b540f9618928ace30cf309c7ff7ab
Stromxun <1512144613@qq.com>
2024-01-22 17:56:14 +0800

    finish lab6

--- proj2/byog/lab5/RandomWorldDemo.java
+++ proj2/byog/lab5/RandomWorldDemo.java
@@ -3,8 +3,10 @@ package byog.lab5;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
-
+import org.junit.Test;
 import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Draws a world that contains RANDOM tiles.
@@ -13,7 +15,7 @@ public class RandomWorldDemo {
     private static final int WIDTH = 50;
     private static final int HEIGHT = 50;
 
-    private static final long SEED = 2873123;
+    private static final long SEED = 1;
     private static final Random RANDOM = new Random(SEED);
 
     /**
@@ -44,14 +46,134 @@ public class RandomWorldDemo {
         }
     }
 
+    public static class Position {
+        public int x;
+        public int y;
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    /**
+     * Computes the width of row i for a size s hexagon.
+     * @param s The size of the hex.
+     * @param i The row number where i = 0 is the bottom row.
+     * @return
+     */
+    public static int hexRowWidth(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+
+        return s + 2 * effectiveI;
+    }
+
+    /**
+     * Computesrelative x coordinate of the leftmost tile in the ith
+     * row of a hexagon, assuming that the bottom row has an x-coordinate
+     * of zero. For example, if s = 3, and i = 2, this function
+     * returns -2, because the row 2 up from the bottom starts 2 to the left
+     * of the start position, e.g.
+     *   xxxx
+     *  xxxxxx
+     * xxxxxxxx
+     * xxxxxxxx <-- i = 2, starts 2 spots to the left of the bottom of the hex
+     *  xxxxxx
+     *   xxxx
+     *
+     * @param s size of the hexagon
+     * @param i row num of the hexagon, where i = 0 is the bottom
+     * @return
+     */
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return -effectiveI;
+    }
+
+    /** Adds a row of the same tile.
+     * @param world the world to draw on
+     * @param p the leftmost position of the row
+     * @param width the number of tiles wide to draw
+     * @param t the tile to draw
+     */
+    public static void addRow(TETile[][] world, Position p, int width, TETile t) {
+        for (int xi = 0; xi < width; xi += 1) {
+            int xCoord = p.x + xi;
+            int yCoord = p.y;
+            world[xCoord][yCoord] = TETile.colorVariant(t, 32, 32, 32, RANDOM);
+        }
+    }
+
+    /**
+     * Adds a hexagon to the world.
+     * @param world the world to draw on
+     * @param p the bottom left coordinate of the hexagon
+     * @param s the size of the hexagon
+     * @param t the tile to draw
+     */
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+
+        if (s < 2) {
+            throw new IllegalArgumentException("Hexagon must be at least size 2.");
+        }
+
+        // hexagons have 2*s rows. this code iterates up from the bottom row,
+        // which we call row 0.
+        for (int yi = 0; yi < 2 * s; yi += 1) {
+            int thisRowY = p.y + yi;
+
+            int xRowStart = p.x + hexRowOffset(s, yi);
+            Position rowStartP = new Position(xRowStart, thisRowY);
+
+            int rowWidth = hexRowWidth(s, yi);
+
+            addRow(world, rowStartP, rowWidth, t);
+
+        }
+    }
+
+    @Test
+    public void testHexRowWidth() {
+        assertEquals(3, hexRowWidth(3, 5));
+        assertEquals(5, hexRowWidth(3, 4));
+        assertEquals(7, hexRowWidth(3, 3));
+        assertEquals(7, hexRowWidth(3, 2));
+        assertEquals(5, hexRowWidth(3, 1));
+        assertEquals(3, hexRowWidth(3, 0));
+        assertEquals(2, hexRowWidth(2, 0));
+        assertEquals(4, hexRowWidth(2, 1));
+        assertEquals(4, hexRowWidth(2, 2));
+        assertEquals(2, hexRowWidth(2, 3));
+    }
+
+    @Test
+    public void testHexRowOffset() {
+        assertEquals(0, hexRowOffset(3, 5));
+        assertEquals(-1, hexRowOffset(3, 4));
+        assertEquals(-2, hexRowOffset(3, 3));
+        assertEquals(-2, hexRowOffset(3, 2));
+        assertEquals(-1, hexRowOffset(3, 1));
+        assertEquals(0, hexRowOffset(3, 0));
+        assertEquals(0, hexRowOffset(2, 0));
+        assertEquals(-1, hexRowOffset(2, 1));
+        assertEquals(-1, hexRowOffset(2, 2));
+        assertEquals(0, hexRowOffset(2, 3));
+    }
     public static void main(String[] args) {
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
 
         TETile[][] randomTiles = new TETile[WIDTH][HEIGHT];
         fillWithRandomTiles(randomTiles);
+        addHexagon(randomTiles, new Position(10, 10), 3, Tileset.SAND);
 
         ter.renderFrame(randomTiles);
+
+
     }
 
 
--- proj2/byog/lab6/MemoryGame.java
+++ proj2/byog/lab6/MemoryGame.java
@@ -1,5 +1,6 @@
 package byog.lab6;
 
+import edu.princeton.cs.introcs.Draw;
 import edu.princeton.cs.introcs.StdDraw;
 
 import java.awt.Color;
@@ -19,17 +20,17 @@ public class MemoryGame {
                                                    "Too easy for you!", "Wow, so impressive!"};
 
     public static void main(String[] args) {
-        if (args.length < 1) {
+        /*if (args.length < 1) {
             System.out.println("Please enter a seed");
             return;
-        }
+        }*/
 
-        int seed = Integer.parseInt(args[0]);
-        MemoryGame game = new MemoryGame(40, 40);
+        int seed = 11;
+        MemoryGame game = new MemoryGame(40, 40, seed);
         game.startGame();
     }
 
-    public MemoryGame(int width, int height) {
+    public MemoryGame(int width, int height, int seed) {
         /* Sets up StdDraw so that it has a width by height grid of 16 by 16 squares as its canvas
          * Also sets up the scale so the top left is (0,0) and the bottom right is (width, height)
          */
@@ -44,31 +45,98 @@ public class MemoryGame {
         StdDraw.enableDoubleBuffering();
 
         //TODO: Initialize random number generator
+        rand = new Random(seed);
     }
 
     public String generateRandomString(int n) {
         //TODO: Generate random string of letters of length n
-        return null;
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            str.append(CHARACTERS[rand.nextInt(CHARACTERS.length)]);
+        }
+        return str.toString();
     }
 
     public void drawFrame(String s) {
+        int midWidth = width / 2;
+        int midHeight = height / 2;
+
+        StdDraw.clear();
+        StdDraw.clear(Color.black);
+
         //TODO: Take the string and display it in the center of the screen
+        Font bigFont = new Font("Monaco", Font.BOLD, 30);
+        StdDraw.setFont(bigFont);
+        StdDraw.setPenColor(Color.white);
+        StdDraw.text(midWidth, midHeight, s);
+
         //TODO: If game is not over, display relevant game information at the top of the screen
+        if (!gameOver) {
+            Font smallFont = new Font("Monaco", Font.BOLD, 20);
+            StdDraw.setFont(smallFont);
+            StdDraw.textLeft(1, height - 1, "Round: " + round);
+            StdDraw.text(midWidth, height - 1, playerTurn ? "Type!" : "Watch!");
+            StdDraw.textRight(width - 1, height - 1, ENCOURAGEMENT[round % ENCOURAGEMENT.length]);
+            StdDraw.line(0, height - 2, width, height - 2);
+        }
+
+        StdDraw.show();
     }
 
     public void flashSequence(String letters) {
         //TODO: Display each character in letters, making sure to blank the screen between letters
+        for (Character ch : letters.toCharArray()) {
+            drawFrame(ch.toString());
+            StdDraw.pause(1000);
+            drawFrame("");
+            StdDraw.pause(500);
+        }
     }
 
     public String solicitNCharsInput(int n) {
         //TODO: Read n letters of player input
-        return null;
+        String input = "";
+        drawFrame(input);
+
+        while (input.length() < n) {
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue;
+            }
+            input += StdDraw.nextKeyTyped();
+            drawFrame(input);
+        }
+        StdDraw.pause(500);
+        return input;
     }
 
     public void startGame() {
         //TODO: Set any relevant variables before the game starts
+        this.gameOver = false;
+        this.playerTurn = false;
+        this.round = 1;
 
         //TODO: Establish Game loop
+        while (!this.gameOver) {
+            playerTurn = false;
+            drawFrame("Round:" + round);
+            StdDraw.pause(2000);
+
+            String expect = generateRandomString(round);
+            flashSequence(expect);
+
+            playerTurn = true;
+            String input = solicitNCharsInput(round);
+
+            if (!input.equals(expect)) {
+                this.gameOver = true;
+                drawFrame("Game Over! Final level: " + round);
+            } else {
+                drawFrame("Correct, well done!");
+                StdDraw.pause(1500);
+                round += 1;
+            }
+        }
     }
 
 }
